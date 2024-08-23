00aaced149aaf21a03879a3ddf162714241219d1
Vladimir Inshakov <markoni48@yandex.ru>
2024-08-23 06:09:19 +0300

    Squashed commit of the following:
    commit 2d12b0aaa235f26d426d3fea0b5f393895cc0fd7
    Author: Vladimir Inshakov <markoni48@yandex.ru>
    Date:   Fri Aug 23 06:08:41 2024 +0300
    
        Добавление средств разработки
    
        Установка и настройка дополнительных средств разработки и тестирования. Создание отображения печатной формы посредством шаблона.
    
    commit 09dffbda8e36dd3e735ca9db116b699307f3c71c
    Author: Vladimir Inshakov <markoni48@yandex.ru>
    Date:   Thu Aug 22 23:38:56 2024 +0300
    
        Добавление тестов
    
        Тестовая страничка проверки дизайна

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,102 @@
-const { series, src, dest } = require('gulp');
+const { watch, series, parallel, src, dest } = require('gulp');
 const rename = require('gulp-rename');
+const clean = require('gulp-clean');
 const pug = require('gulp-pug');
 const sass = require('gulp-sass')(require('sass'));
 const htmlmin = require('gulp-htmlmin');
+const transform = require('gulp-transform');
+const fs = require('fs');
+const replace = require('@stdlib/string-replace');
+const webserver = require('gulp-webserver');
+
+// Настройки директорий
+var dir = {
+  build: 'dist/',
+  tempout: "output/",
+  test: 'test/',
+  src: {
+    pugs: 'src-forms/*.pug',
+    sass: 'src-forms/**/*.sass'
+    }
+}
 
 
 // Задача `clean` очистка от всех временных файлов
-function clean(cb) {
-  // TODO: очистка директории
-  console.log("Clean - Еще не реализовано");
-  cb();
+function cleanFiles() {
+  console.log("Clean directories");
+  return src([dir.tempout, dir.build], {read: false, allowEmpty: true})
+    .pipe(clean({force: true}));
 }
 
 // Задача конвертирование и минимизация SASS->CSS
 function buildStyles() {
-  return src('src-forms/**/*.sass')
+  return src(dir.src.sass)
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(dest('output'));
-};
+    .pipe(dest(dir.tempout));
+}
 
 // Задача минимизация HTML кода
 function htmlMinify() {
-  return src('output/**/*.html.txt')
+  return src(dir.tempout + '**/*.html')
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(dest('dist'));
-};
+    .pipe(rename({ extname: '.html.txt' }))
+    .pipe(dest(dir.build));
+}
 
 // Задача конвертирование файлов PUG->HTML
 function pug2htmlConvert() {
 
-  return src('src-forms/*.pug')
+  return src(dir.src.pugs)
     .pipe(pug({
       data: {name: 'Modest'}
     }))
-    .pipe(rename({ extname: '.html.txt' }))
-    .pipe(dest('output'));
+    .pipe(rename({ extname: '.html' }))
+    .pipe(dest(dir.tempout));
+}
+
+// Подзадача тестирования. Создание визуализации по шаблону.
+function testFormCreate(){
+
+  var template = fs.readFileSync(dir.test + 'printed_form_okdesk_template.html','utf8');
+
+  return src(dir.tempout + '**/*.html')
+    .pipe(transform('utf8',(content, file) => {
+        return replace(template, '%CUSTOM_FORM_PLACER%', content);
+    }))
+    .pipe(dest(dir.tempout + dir.test));
+}
+
+// Подзадача тестирования. Копирование сопутствующих файлов теста.
+function testOthersCopy(){
+  return src(dir.test + '*.css').pipe(dest(dir.tempout + dir.test));
+}
+
+// Наблюдать за исходными файлами и компиллировать их на лету при изменении Ctrl+C завершить работу
+// Перед первым запуском выполнить команду build
+function watching () {
+  console.log("Watching files PUGs, SASS'...");
+  watch(dir.src.pugs, { queue: false }, series(pug2htmlConvert, testFormCreate));
+  watch(dir.src.sass, { queue: false }, buildStyles);
+}
+
+// Запуск локального веб-сервера для проверки отображения форм Ctrl+C завершить работу
+// Перед первым запуском выполнить команду test
+function webServer() {
+  return src('.')
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: true,
+      open: dir.tempout + dir.test,
+      port: 8080
+    }))
 }
 
 
-function defaultTask(cb) {
-  // TODO: Поместить заданич по-умолчанию
-  console.log("defaultTask - Еще не реализовано");
-  cb();
-}
+exports.clean = cleanFiles;
+exports.build = series(cleanFiles, buildStyles, pug2htmlConvert, htmlMinify);
+exports.test = series(buildStyles, pug2htmlConvert, testOthersCopy, testFormCreate);
+exports.webserver = webServer;
+exports.watch = watching;
 
-exports.clean = clean;
-exports.buildStyles = buildStyles;
-exports.build = series(clean, buildStyles, pug2htmlConvert, htmlMinify);
-exports.default = defaultTask;
-
-exports.watch = function () {
-  gulp.watch('src-forms/**/*.sass', ['sass']);
-};
+// Watching files (live compiling)
+exports.default = parallel(webServer, watching);
